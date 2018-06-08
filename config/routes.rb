c28656d24b01f35e3f902418f7e5be515f627e2f
Solace <solace@Solaces-MacBook.local>
2018-06-08 17:04:46 +0800

    Second google API

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
   get "/sign_up" => "users#new", as: "sign_up"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
+  # root "sessions#new"
+
+  root "home#index"
 end
